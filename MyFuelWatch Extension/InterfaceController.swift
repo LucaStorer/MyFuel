2a9252374796f3cf872cf127cfebd28e5603e537
LucaStorer <lucastorer86@gmail.com>
2017-01-04 01:55:06 +0100

    Add Support Apple Watch

---
+++ MyFuelWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MyFuelWatch Extension
+//
+//  Created by Luca Storer on 04/01/17.
+//  Copyright © 2017 Luca Storer. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
