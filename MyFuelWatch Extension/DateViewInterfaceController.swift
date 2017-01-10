2d7eead8c2e242b64e375cc33857af564dabed87
LucaStorer <lucastorer86@gmail.com>
2017-01-10 11:08:15 +0100

    sistemato vista scelta data

--- MyFuelWatch Extension/DateViewInterfaceController.swift
+++ MyFuelWatch Extension/DateViewInterfaceController.swift
@@ -12,7 +12,131 @@ import Foundation
 
 class DateViewInterfaceController: WKInterfaceController {
 
-    @IBOutlet var picDay: WKInterfacePicker!
+var monthsMap = [Int: String]()
+	var daysArray = [Int]()
+	var yearsArray = [Int]()
+
+	
+	var month = 1
+	var day = 1
+	var year = Int()
+	
+
+	let yearOffset = 1900
+	
+	@IBOutlet var monthPicker: WKInterfacePicker!
+	@IBAction func monthPicker(value: Int) {
+		month = value + 1
+	}
+	
+	@IBOutlet var dayPicker: WKInterfacePicker!
+	@IBAction func dayPicker(value: Int) {
+		day = value + 1
+	}
+	
+	@IBOutlet var yearPicker: WKInterfacePicker!
+	@IBAction func yearPicker(value: Int) {
+		year = yearsArray[value]
+	}
+	
+	
+	func getDateFromString() -> NSDate {
+		let dateFormatter = NSDateFormatter()
+		dateFormatter.dateFormat = "M d yyyy"
+		
+		let dateString = month.description + " " + day.description + " " + year.description 
+		let dateObtainedFromString = dateFormatter.dateFromString(dateString)
+		
+		if let _ = dateObtainedFromString {
+			return dateObtainedFromString!
+		}
+		else {
+			return NSDate.init(timeIntervalSinceNow: 100)
+		}
+	}
+	
+	override func awakeWithContext(context: AnyObject?) {
+		// Fill months map with month abbreviations
+		monthsMap[0] = "Jan"
+		monthsMap[1] = "Feb"
+		monthsMap[2] = "Mar"
+		monthsMap[3] = "Apr"
+		monthsMap[4] = "May"
+		monthsMap[5] = "Jun"
+		monthsMap[6] = "Jul"
+		monthsMap[7] = "Aug"
+		monthsMap[8] = "Sep"
+		monthsMap[9] = "Oct"
+		monthsMap[10] = "Nov"
+		monthsMap[11] = "Dec"
+		
+		// Fill monthPicker with month abbreviation strings
+		var monthPickerItems = [WKPickerItem]()
+		for var i = 0; i < monthsMap.count; ++i {
+			let pickerItem = WKPickerItem()
+			pickerItem.title = monthsMap[i]
+			pickerItem.caption = "Month"
+			monthPickerItems.append(pickerItem)
+		}
+		
+		monthPicker.setItems(monthPickerItems)
+		
+		// Create and initialize daysArray and daysPicker
+		var day = 1
+		
+		var dayPickerItems = [WKPickerItem]()
+		for var i = 0; i < 31; ++i {
+			daysArray.append(day++)
+			let pickerItem = WKPickerItem()
+			pickerItem.title = daysArray[i].description
+			pickerItem.caption = "Day"
+			dayPickerItems.append(pickerItem)
+		}
+		
+		dayPicker.setItems(dayPickerItems)
+		
+		for var yearIndex = 1900; yearIndex <= 2100; ++yearIndex {
+			yearsArray.append(yearIndex)
+		}
+		var yearPickerItems = [WKPickerItem]()
+		for var i = 0; i < yearsArray.count; ++i {
+			let pickerItem = WKPickerItem()
+			pickerItem.title = yearsArray[i].description
+			pickerItem.caption = "Year"
+			yearPickerItems.append(pickerItem)
+		}
+		
+		yearPicker.setItems(yearPickerItems)
+		
+		// Set picker defaults so user doesn't have to manually scroll
+		setDefaultPickersOnDatePickerInterfaceController()
+		
+		year = yearsArray[0]
+	}
+	
+	func setDefaultPickersOnDatePickerInterfaceController() {
+		let todayDate = NSDate()
+		let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+		
+		let monthComponent = calendar.components(.Month, fromDate: todayDate)
+		let month = monthComponent.month
+		
+		let dayComponent = calendar.components(.Day, fromDate: todayDate)
+		let day = dayComponent.day
+		
+		let yearComponent = calendar.components(.Year, fromDate: todayDate)
+		let year = yearComponent.year
+		
+				
+		// These have no "0" state so they are 0 index based
+		monthPicker.setSelectedItemIndex(month - 1)
+		dayPicker.setSelectedItemIndex(day - 1)
+		yearPicker.setSelectedItemIndex(year - yearOffset)
+		
+	}
+
+
+  /*  @IBOutlet var picDay: WKInterfacePicker!
     @IBOutlet var picMonth: WKInterfacePicker!
     @IBOutlet var picYear: WKInterfacePicker!
     
@@ -116,7 +240,7 @@ class DateViewInterfaceController: WKInterfaceController {
         
         presentController(withName: "newitemview", context: DateString)
         
-    }
+    }*/
     
     
 }
